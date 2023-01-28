648fa74e27372507dc60034a1dd397fc2e56d43a
adersoncarvalho <aderson_carvalho@hotmail.com>
2023-01-28 12:14:59 -0300

    Criando formulas % de betume #3

---
+++ js/esxtracaoBetume.js
@@ -0,0 +1,50 @@
+//porcentagem
+function porcentagem(a, b) {
+    return (a / b) * 100;
+}
+
+//mostrar resultados
+function mostrarValor(valor) {
+    console.log(valor.toFixed(2));
+}
+
+//somar valores
+function soma(a, b) {
+    return parseFloat(a + b);
+}
+
+
+//peso do Betume
+function pesoBetume() {
+    const pesoAmostraCBetume = parseFloat(
+        document.getElementById("peso-amostra-com-betume").value
+    );
+
+    const pesoAmostraSBetume = parseFloat(
+        document.getElementById("peso-amostra-sem-betume").value
+    );
+
+    const pesoBetume = pesoAmostraCBetume - pesoAmostraSBetume;
+    document.getElementById("peso-do-betume").value = parseFloat(
+        pesoBetume.toFixed(2)
+    );
+}
+
+
+//% do Betume
+function porcentagemBetume() {
+    const pesoAmostraCBetume = parseFloat(
+        document.getElementById("peso-amostra-com-betume").value
+    );
+    
+    const pesoBetume = parseFloat(
+        document.getElementById("peso-do-betume").value
+    );
+
+    const porcentagemBetume = porcentagem(pesoBetume, pesoAmostraCBetume);
+
+    document.getElementById("porcentagem-do-betume").value = parseFloat(
+        porcentagemBetume.toFixed(2)
+    );
+
+}
