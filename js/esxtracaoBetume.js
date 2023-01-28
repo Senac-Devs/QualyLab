6da399329eb8d242d9e6b4d404b8d60e6ccda38e
maycanin <maysoninacio@gmail.com>
2023-01-28 19:30:40 -0300

    grafico

--- js/esxtracaoBetume.js
+++ js/esxtracaoBetume.js
@@ -1,49 +1,48 @@
 //porcentagem
 function porcentagem(a, b) {
-    return (a / b) * 100;
+  return (a / b) * 100
 }
 
 //mostrar resultados
 function mostrarValor(valor) {
-    console.log(valor.toFixed(2));
+  console.log(valor.toFixed(2))
 }
 
 //somar valores
 function soma(a, b) {
-    return parseFloat(a + b);
+  return parseFloat(a + b)
 }
-
 
 //peso do Betume
 function pesoBetume() {
-    const pesoAmostraCBetume = parseFloat(
-        document.getElementById("peso-amostra-com-betume").value
-    );
+  const pesoAmostraCBetume = parseFloat(
+    document.getElementById('peso-amostra-com-betume').textContent,
+  )
 
-    const pesoAmostraSBetume = parseFloat(
-        document.getElementById("peso-amostra-sem-betume").value
-    );
+  const pesoAmostraSBetume = parseFloat(
+    document.getElementById('peso-amostra-sem-betume').textContent,
+  )
 
-    const pesoBetume = pesoAmostraCBetume - pesoAmostraSBetume;
-    document.getElementById("peso-do-betume").value = parseFloat(
-        pesoBetume.toFixed(2)
-    );
+  const pesoBetume = pesoAmostraCBetume - pesoAmostraSBetume
+
+  document.getElementById('peso-do-betume').value = parseFloat(
+    pesoBetume.toFixed(2),
+  )
+  console.log(pesoAmostraSBetume)
+  porcentagemBetume()
 }
-
 
 //% do Betume
 function porcentagemBetume() {
-    const pesoAmostraCBetume = parseFloat(
-        document.getElementById("peso-amostra-com-betume").value
-    );
-    
-    const pesoBetume = parseFloat(
-        document.getElementById("peso-do-betume").value
-    );
+  const pesoAmostraCBetume = parseFloat(
+    document.getElementById('peso-amostra-com-betume').textContent,
+  )
 
-    const porcentagemBetume = porcentagem(pesoBetume, pesoAmostraCBetume);
+  const pesoBetume = parseFloat(document.getElementById('peso-do-betume').value)
 
-    document.getElementById("porcentagem-do-betume").value = 
-        porcentagemBetume.toFixed(2);
+  const porcentagemBetume = porcentagem(pesoBetume, pesoAmostraCBetume)
 
+  document.getElementById(
+    'porcentagem-do-betume',
+  ).value = porcentagemBetume.toFixed(2)
 }
